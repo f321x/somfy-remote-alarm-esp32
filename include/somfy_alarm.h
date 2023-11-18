749e8ff54ff22e753264c889a53c4a3a2c73fa03
f321x <f321x@tutamail.com>
2023-11-18 19:50:17 +0100

    test

--- include/somfy_alarm.h
+++ include/somfy_alarm.h
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-
+#include "website.h"
 #include <EEPROM.h>
 #include <EEPROMRollingCodeStorage.h>
 #include <SomfyRemote.h>
@@ -16,7 +16,7 @@
 #define REMOTE 0x5184c8
 #define CC1101_FREQUENCY 433.42
 
-#define WIFI_SSID "Steuern sind Raub"
-#define WIFI_PW "npsr2azPmddm"
+#define WIFI_SSID ""
+#define WIFI_PW ""
 
 // https://github.com/Legion2/Somfy_Remote_Lib
--- src/main.cpp
+++ src/main.cpp
@@ -4,58 +4,7 @@ ESP32Time rtc(3600);
 AsyncWebServer server(80);
 
 const char* PARAM_MESSAGE = "param message";
-const char* WEBSITE =  R"rawliteral(<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alarm Clock</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-            margin-top: 50px;
-        }
-        #alarmTime {
-            font-size: 1.5em;
-        }
-</style>
-<script>
-    function setAlarm() {
-        var timeInput = document.getElementById('timeInput').value;
-        document.getElementById('alarmTime').innerText = timeInput;
 
-        var data = { time: timeInput };
-            fetch('/alarm-time-set', {
-            method: 'POST',     // HTTP method
-            headers: {
-                'Content-Type': 'application/json',  // Data type being sent
-            },
-            body: JSON.stringify(data),  // Convert the JavaScript object to a JSON string
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-                return response.json();  // or response.text() if the response is not in JSON format
-            })
-            .then(data => {
-                console.log('Success:', data);  // Handle success here
-            })
-            .catch((error) => {
-                console.error('Error:', error);  // Handle errors here
-            })};
-</script>
-</head>
-<body>
-    <h1>Somfy Alarm Clock</h1>
-    <p>Set the alarm time:</p>
-    <input type="time" id="timeInput">
-    <button onclick="setAlarm()">Set Alarm</button>
-    <h2>Current Alarm Time: <span id="alarmTime"></span></h2>
-</body>
-</html>
-)rawliteral";
 
 void notFound(AsyncWebServerRequest *request);
 
